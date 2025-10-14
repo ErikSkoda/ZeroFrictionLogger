dd4d33040de2f4334dde5bba7643ed82e5e50be4
Erik <e.skoda@chello.nl>
2025-10-14 15:43:10 +0200

    Update PascalToSentence.cs
    View the CHANGELOG.md for the complete list of Version 1.1.0 updates.

--- test/ZeroFrictionLogger.Tests/PascalToSentence.cs
+++ test/ZeroFrictionLogger.Tests/PascalToSentence.cs
@@ -27,11 +27,18 @@
 using System.Text;
 using System.Linq; // required to make lib #legacy #compliant with .Net Core 2.1 (Out of support)
 
-// namespace ZeroFrictionLogger; // notation not #compliant with #legacy .Net Core 2.1
 namespace ZeroFrictionLogger // #compliant with both #legacy .Net Core 2.1 and .Net Core 8.0 LTS
 {
     internal static class PascalToSentence // eliminates #dependency on Humanizer package
     {
+        private const int _firstCharacterIndex = 0;
+        private const int _secondCharacterIndex = 1;
+        private const int _lastCharacterOffset = -1;
+        private const int _previousCharacterOffset = 1;
+        private const int _nextCharacterOffset = 1;
+        private const int _secondPreviousCharacterOffset = 2;
+        private const char _spaceChar = ' ';
+
         // #logic lives below here, sort of like a formularium in insurance industry.
         private static bool IsLogicalToAddSpaceForPascalCase(int i, string value)
             => IsUpperCase(value[i])
@@ -62,24 +69,27 @@ namespace ZeroFrictionLogger // #compliant with both #legacy .Net Core 2.1 and .
             && !IsExclusiveLogicLowerCase(i, valueWithSpacesInserted); // << explicit (must have)
 
         // #helpers for logic readability below here.
-        private static bool IsFirstCharacter(int i) => i == 0;
+        private static bool IsFirstCharacter(int i) => i == _firstCharacterIndex;
         private static bool NotFirstCharacter(int i) => !IsFirstCharacter(i);
-        private static bool IsSecondCharacter(int i) => i == 1;
-        private static bool IsLastCharacter(int i, string value) => i == value.Length - 1;
+        private static bool IsSecondCharacter(int i) => i == _secondCharacterIndex;
+        private static bool IsLastCharacter(int i, string value)
+            => i == value.Length + _lastCharacterOffset;
         private static bool NotLastCharacter(int i, string value) => !IsLastCharacter(i, value);
         private static bool IsUpperCase(char c) => char.IsUpper(c);
         private static bool IsPreviousUpperCase(int i, string value)
-            => !IsFirstCharacter(i) && char.IsUpper(value[i - 1]);
+            => !IsFirstCharacter(i) && char.IsUpper(value[i - _previousCharacterOffset]);
         private static bool NotPreviousUpperCase(int i, string value)
             => !IsPreviousUpperCase(i, value);
         private static bool IsPreviousCharSpace(int i, string value)
-            => !IsFirstCharacter(i) && ((value[i - 1] == ' '));
+            => !IsFirstCharacter(i) && (value[i - _previousCharacterOffset] == _spaceChar);
         private static bool IsSecondPreviousUpperCase(int i, string value)
-            => !IsFirstCharacter(i) && !IsSecondCharacter(i) && char.IsUpper(value[i - 2]);
+            => !IsFirstCharacter(i)
+                && !IsSecondCharacter(i)
+                && char.IsUpper(value[i - _secondPreviousCharacterOffset]);
         private static bool IsNextUpper(int i, string value)
-            => NotLastCharacter(i, value) && char.IsUpper(value[i + 1]);
+            => NotLastCharacter(i, value) && char.IsUpper(value[i + _nextCharacterOffset]);
         private static bool IsNextLowerCase(int i, string value)
-            => NotLastCharacter(i, value) && char.IsLower(value[i + 1]);
+            => NotLastCharacter(i, value) && char.IsLower(value[i + _nextCharacterOffset]);
         private static bool NotNextUpperCase(int i, string value) => !IsNextUpper(i, value);
 
         private static bool[] GetNeedForSpace(string value) =>
@@ -96,7 +106,7 @@ namespace ZeroFrictionLogger // #compliant with both #legacy .Net Core 2.1 and .
             {
                 if (shouldIncludeSpace[i]) AppendCharWithSpacePrefix(sb, value[i]);
                 if (shouldNotIncludeSpace[i]) AppendCharAsIs(sb, value[i]);
-            }   // ^^ enforced mutually exclusive if statements
+            }   // ^^ enforced mutually exclusive if statements, replacing an else
             return sb.ToString();
         }
 
@@ -114,15 +124,15 @@ namespace ZeroFrictionLogger // #compliant with both #legacy .Net Core 2.1 and .
         }
 
         private static void AppendCharInLowerCase(StringBuilder sb, char value)
-            => sb.Append(value.ToString().ToLower());
+            => sb.Append(value.ToString().ToLowerInvariant());
         private static void AppendCharInUpperCase(StringBuilder sb, char value)
-            => sb.Append(value.ToString().ToUpper());
+            => sb.Append(value.ToString().ToUpperInvariant());
         //private static void AppendUnderScoreForDebugging(StringBuilder sb)
         //    => sb.Append('_'); // #debug
         private static void AppendCharAsIs(StringBuilder sb, char value) => sb.Append(value);
         private static void AppendCharWithSpacePrefix(StringBuilder sb, char value)
         {
-            sb.Append(' ');
+            sb.Append(_spaceChar);
             sb.Append(value);
         }
 
@@ -140,7 +150,7 @@ namespace ZeroFrictionLogger // #compliant with both #legacy .Net Core 2.1 and .
             }
             catch
             {
-                return value;
+                return value; // fallback to passed param in case of an exception.
             }
         }
     }
